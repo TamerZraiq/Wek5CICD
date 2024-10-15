5f5c388f91a8b91ff709edbf84d9d9aaaf9d919c
Tamer Zraiq <G00425053@atu.ie>
2024-10-15 10:03:48 +0100

    New Project

---
+++ src/main/java/ie/atu/week5cicd/Week5CicdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5CicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5CicdApplication.class, args);
+    }
+
+}
