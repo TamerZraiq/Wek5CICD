0fbfd117d51c33d86d732b767fae835323388738
Tamer Zraiq <G00425053@atu.ie>
2024-10-15 11:16:44 +0100

    Edited the classes

--- src/main/java/ie/atu/week5cicd/Person.java
+++ src/main/java/ie/atu/week5cicd/Person.java
@@ -1,9 +1,6 @@
 package ie.atu.week5cicd;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +11,22 @@ import lombok.NoArgsConstructor;
 public class Person {
     @NotBlank(message = "Name cannot be Blank")
     private String name;
+
     @Email(message = "Invalid email Address")
     private String email;
+
     @Min(value = 1, message = "Age must be greater than or equal to 1")
     private int age;
+
     @NotBlank(message = "Title cannot be Blank")
-    @Size(max = 13, message ="Title cannot be greater than 13 characters")
     private String title;
-    @NotBlank(message = "Employee ID cannot be Blank")
-    @Size(max = 13, message ="ID cannot be greater than 13 characters")
-    private String id;
+
+    @NotNull(message ="ID cannot be greater than 15 characters")
+    private int id;
+
     @NotBlank(message = "Position cannot be blank")
     private String position;
+
     @NotBlank(message = "Department cannot be blank")
     private String department;
 
